3001ea090e5930019e8594eb5996e32bda6fcf18
Tymofii Shyshkovtsov <tima.shishkovtsov@gmail.com>
2023-08-30 12:58:00 -0400

    main fix

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,7 +5,7 @@ import org.example.Tictactoe.ttGameLoop;
 public class Main {
     public static void main(String[] args) {
         //QuizLogic quizLogic = new QuizLogic();
-        //quizLogic.inputAndOutput();
+        //quizLogic.run();
         ttGameLoop tictactoe = new ttGameLoop();
     }
 }
